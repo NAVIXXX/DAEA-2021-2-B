f410acb4a28de2505eb83097fa68c884cb756976
NAVIXXX <jose.aimituma@tecsup.edu.pe>
2021-08-27 23:40:58 -0500

    Tarea 1

---
+++ lab01/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+
+    
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgvUsuarios_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAgregar_Click_1(object sender, EventArgs e)
+        {
+            
+            string codigo = "";
+            string nombre = txtNombre.Text;
+            string dni = txtDNI.Text;
+            string apellidos = txtApellidos.Text;
+            string direccion = txtDireccion.Text;
+            string telefono = txtTelefono.Text;
+            string email = txtEmail.Text;
+            string fecnac = dtpNac.Text;
+            int index = cbDepart.SelectedIndex;
+            string departamento = cbDepart.Items[index].ToString();
+            int index2 = cbCargo.SelectedIndex;
+            string cargo = cbCargo.Items[index2].ToString();
+            dgvUsuarios.Rows.Add(codigo, nombre, dni, apellidos, direccion, telefono, email, fecnac, departamento, cargo);
+
+        }
+    }
+}
--- lab01/prjLAB01-2/Form1.cs
+++ lab01/prjLAB01-2/Form1.cs
@@ -17,18 +17,55 @@ namespace prjLAB01_2
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void btnSumar_Click(object sender, EventArgs e)
         {
+            Double valor1 = Convert.ToInt32(txtNumero1.Text);
+            Double valor2 = Convert.ToInt32(txtNumero2.Text);
+            Double suma = valor1 + valor2;
+            txtSuma.Text = Convert.ToString(suma);
 
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void btnRestar_Click(object sender, EventArgs e)
         {
+            Double valor1 = Convert.ToInt32(txtNumero1.Text);
+            Double valor2 = Convert.ToInt32(txtNumero2.Text);
+            Double resta = valor1 - valor2;
+            txtResta.Text = Convert.ToString(resta);
 
         }
 
-        private void label2_Click(object sender, EventArgs e)
+        private void btnMultiplicar_Click(object sender, EventArgs e)
         {
+            Double valor1 = Convert.ToInt32(txtNumero1.Text);
+            Double valor2 = Convert.ToInt32(txtNumero2.Text);
+            Double multiplicacion = valor1 * valor2;
+            txtMultiplicacion.Text = Convert.ToString(multiplicacion);
+
+        }
+
+        private void btnDividir_Click(object sender, EventArgs e)
+        {
+            Double valor1 = Convert.ToInt32(txtNumero1.Text);
+            Double valor2 = Convert.ToInt32(txtNumero2.Text);
+            Double division = valor1 / valor2;
+            txtDivision.Text = Convert.ToString(division);
+
+        }
+
+        private void btnCelFar_Click(object sender, EventArgs e)
+        {
+            Double valor1 = Convert.ToInt32(txtTemperatura.Text);
+            double res = ((9 * valor1) / 5) + 32;
+            ResulTemp.Text = Convert.ToString(res);
+
+        }
+
+        private void btnFarCel_Click(object sender, EventArgs e)
+        {
+            Double valor1 = Convert.ToInt32(txtTemperatura.Text);
+            double res = (5 * (valor1 - 32)) / 9;
+            ResulTemp.Text = Convert.ToString(res);
 
         }
     }
