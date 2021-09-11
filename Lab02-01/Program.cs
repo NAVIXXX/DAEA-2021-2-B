a08257c70289d5d36eac550a0a7d7b247a7ce963
NAVIXXX <jose.aimituma@tecsup.edu.pe>
2021-09-10 23:26:16 -0500

    laboratorio 2

--- Lab02-01/Program.cs
+++ Lab02-01/Program.cs
@@ -1,23 +1,22 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace prjLAB01_2
+namespace Lab02_01
 {
     static class Program
     {
         /// <summary>
-        ///  The main entry point for the application.
+        /// Punto de entrada principal para la aplicación.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmLogin());
         }
     }
 }
