151ca2da6c24a9ee503e2ed999743f643108429a
NAVIXXX <jose.aimituma@tecsup.edu.pe>
2021-10-11 12:43:54 -0500

    desarrollo lab07

---
+++ lab07/Negocio/clsNegPerson.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datos;
+using System.Data;
+
+namespace Negocio
+{
+    public class clsNegPerson
+    {
+        clsDAOPerson dAOPerson = new clsDAOPerson();
+
+        public DataTable GetAll()
+        {
+            return dAOPerson.GetAll();
+        }
+    }
+}
